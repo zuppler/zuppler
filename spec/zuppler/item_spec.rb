38b539e524814474c6628ffe2f5a5c761e1b3aaf
Iulian Costan <iulian.costan@gmail.com>
2015-08-28 13:14:46 +0300

    Fix handle call in item spec.

--- spec/zuppler/item_spec.rb
+++ spec/zuppler/item_spec.rb
@@ -9,7 +9,7 @@ describe Zuppler::Item do
     subject.name, subject.price = 'pizza', 9.99
     expect(subject).to receive(:items_url).and_return('http://example.com')
     expect(subject).to receive(:execute_post).with('http://example.com', item: { 'name' => 'pizza', 'price' => 9.99 })
-    expect(subject).to receive(:v3_success?).twice.and_return(false)
+    expect(subject).to receive(:handle).and_return(false)
     expect(subject.save).to eql(false)
   end
 end
