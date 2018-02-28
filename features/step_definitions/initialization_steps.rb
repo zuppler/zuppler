37702b0dea75de1ba308375fbfbabf5e7811a50a
Iulian Costan <iulian.costan@gmail.com>
2018-02-28 15:39:19 +0530

    Record / fix user cassettes

--- features/step_definitions/initialization_steps.rb
+++ features/step_definitions/initialization_steps.rb
@@ -14,6 +14,7 @@ end
 Given(/^Zuppler is loaded$/) do
   Zuppler.is_a?(Module).should be_truthy
 end
+
 Given(/^Zuppler configured with "(.*)" and "(.*)"$/) do |channel, key|
   Zuppler.configure do |config|
     config.channel_key = channel
@@ -23,15 +24,13 @@ Given(/^Zuppler configured with "(.*)" and "(.*)"$/) do |channel, key|
     # config.domain = 'zuppler.dev'
   end
 end
+
 Given(/^Zuppler application token$/) do
-  # Test Oauth app
-  # za = Zuppler::Application.new 'fd61a191c8b26b364b03eacac66913088abad7e5d144cf0e319d553335e6bbc3', '45669935c634a6cf28f630384cdd6d5bd490983f207005126a9fa1a52fcbbaf6'
-  # @access_token = za.access_token 'admin'
-  @access_token = '78aadbcbf15bb7d393c152a4a8a230f0d0183b4ef62f898c165edc9c6928e693'
+  @access_token = 'ea82ba7cbd993a72bbfbcc80e8d35ea71b659e8c41e5eacf137c42e4f7263d76'
 end
 
 Given(/^Zuppler user token$/) do
-  @access_token = 'cebb85da137eeb8fb264b2a652831b84f65a49f358f502f97596358e347ca18d'
+  @access_token = '1f8639b026344721bd0126b039609835ffa931b44446907acc11a07719fce59a'
 end
 
 #
@@ -46,9 +45,11 @@ When(/^I initialize with (.*) and (.*)$/) do |channel, key|
     # config.domain = 'zuppler.dev'
   end
 end
+
 When(/^Zuppler is not initialized$/) do
   Zuppler.init '', ''
 end
+
 When(/^Zuppler is initialized$/) do
   Zuppler.channel.should be
   Zuppler.api_key.should be
