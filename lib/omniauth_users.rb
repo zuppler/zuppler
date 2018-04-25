4681eaf273fb11bb0d7d591d7109872b277042f5
Bogdan Buliga <bogdan@192-168-0-101.rdsnet.ro>
2018-04-25 16:28:07 +0300

    added theme param

--- lib/omniauth_users.rb
+++ lib/omniauth_users.rb
@@ -26,6 +26,7 @@ module OmniAuth
         options[:authorize_params][:provider] = request.params['provider']
         options[:authorize_params][:provider_page] = request.params['provider_page']
         options[:authorize_params][:locale] = request.params['locale'] if request.params['locale'].present?
+        options[:authorize_params][:theme] = request.params['theme'] if request.params['theme'].present?
         super
       end
 
