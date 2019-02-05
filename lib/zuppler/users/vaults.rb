9c6e5e00273d38ecc3ead04feb671ac35d55a939
Ravindra Yadav <ravindra9278@gmail.com>
2019-02-05 16:35:17 +0530

    added customer_token params in Vault

--- lib/zuppler/users/vaults.rb
+++ lib/zuppler/users/vaults.rb
@@ -12,7 +12,7 @@ module Zuppler
       end
 
       def create_vault(options)
-        requires! options, :name, :brand, :number, :expiration_date, :uid
+        requires! options, :name, :brand, :number, :expiration_date, :uid, :customer_token
         vault_attributes = { vault: options }
         response = execute_post user_vaults_url, vault_attributes, request_headers
         success = v4_success? response
